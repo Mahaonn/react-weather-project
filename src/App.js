1604adc65afb4ef7e9c5adcf2c9c58ecda88da14
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-06-20 20:14:28 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather React App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather React App</h1>
+        <footer>
+          This project was coded by {""}{" "}
+          <a
+            href="https://mywebdevjourney.netlify.app/about"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Yevheniia Kovzikova
+          </a>{" "}
+          and is
+          {""}{" "}
+          <a
+            href="https://github.com/Mahaonn/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            and open-soursed on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
